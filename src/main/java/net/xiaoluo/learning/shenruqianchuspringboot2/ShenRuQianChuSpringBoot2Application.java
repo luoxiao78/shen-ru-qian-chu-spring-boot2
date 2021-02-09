a8e4b5dcf1ca83fe68c13108b5ee7e7332fb2733
Xiao <luoxiao.qc@gmail.com>
2021-02-09 14:21:27 -0500

    build: 初始化项目

---
+++ src/main/java/net/xiaoluo/learning/shenruqianchuspringboot2/ShenRuQianChuSpringBoot2Application.java
@@ -0,0 +1,13 @@
+package net.xiaoluo.learning.shenruqianchuspringboot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShenRuQianChuSpringBoot2Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ShenRuQianChuSpringBoot2Application.class, args);
+  }
+
+}
